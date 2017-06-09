644274391e3a047372fe84dc73eb743dd8018399
viktmv <viktmv@icloud.com>
2017-06-09 19:02:10 +0000

    Add destroy button

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,21 +1,35 @@
 class ReviewsController < ApplicationController
+  before_filter :authorize
+
   def create
     @product = Product.find(params[:product_id])
 
-    p review_params
-
     if current_user.blank?
-      redirect_to '/'
+      redirect_to '/login'
     else
     @product.reviews.create!(
       user: current_user,
       description: review_params[:description],
       rating: review_params[:rating]
     )
-    render '/products/show', product: @product
+    redirect_to product_path(params[:product_id])
     end
   end
 
+
+  def destroy
+    Review.find(params[:id]).destroy
+    respond_to do |format|
+      format.html { redirect_to product_path(params[:product_id]), notice: 'review was successfully destroyed.' }
+    end
+  end
+
+  private
+  def authorize
+    unless current_user.present?
+      redirect_to '/login'
+    end
+  end
 
   def review_params
     params.require(:review).permit(:description, :rating)
